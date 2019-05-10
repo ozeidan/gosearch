17f9e0ad9de67a69545e30fc2dde399bc755e3e8
Omar Zeidan <omar@zeidan.io>
2019-05-10 23:06:43 +0200

    More space efficient in-memory directory tree

--- pkg/tree/tree.go
+++ pkg/tree/tree.go
@@ -5,9 +5,16 @@ import (
 	"strings"
 )
 
+type fileEntry struct {
+	name string
+	node *Node
+}
+
 // Node is a node of a directory tree
 type Node struct {
-	children map[string]Node
+	children []fileEntry
+	part     string
+	parent   *Node
 }
 
 // ErrInvalidPath is returned when the path given to one of
@@ -20,14 +27,34 @@ func (err ErrInvalidPath) Error() string {
 	return fmt.Sprintf("accesing invalid path: %s", err.path)
 }
 
+func (t Node) findFile(name string) (*Node, bool) {
+	for _, c := range t.children {
+		if c.name == name {
+			return c.node, true
+		}
+	}
+	return nil, false
+}
+
+func (t *Node) deleteFile(name string) bool {
+	for i := range t.children {
+		if t.children[i].name == name {
+			t.children[i] = t.children[len(t.children)-1]
+			t.children = t.children[:len(t.children)-1]
+			return true
+		}
+	}
+	return false
+}
+
 // GetChildren returns the directoryies/files of a directory
 // determiend by path
-func (t Node) GetChildren(path string) ([]string, error) {
+func (t *Node) GetChildren(path string) ([]string, error) {
 	parts := pathToParts(path)
 	current := t
 
 	for _, part := range parts {
-		if child, ok := current.children[part]; ok {
+		if child, ok := current.findFile(part); ok {
 			current = child
 		} else {
 			return nil, ErrInvalidPath{path}
@@ -35,27 +62,30 @@ func (t Node) GetChildren(path string) ([]string, error) {
 	}
 
 	keys := make([]string, 0, len(current.children))
-	for key := range current.children {
-		keys = append(keys, key)
+	for _, child := range current.children {
+		keys = append(keys, child.name)
 	}
 
 	return keys, nil
 }
 
 // Add adds a directory to the directory tree
-func (t *Node) Add(path string) {
+func (t *Node) Add(path string) *Node {
 	parts := pathToParts(path)
-	current := *t
+	current := t
 
 	for _, part := range parts {
-		if child, ok := current.children[part]; ok {
+		if child, ok := current.findFile(part); ok {
 			current = child
 		} else {
-			child = Node{make(map[string]Node, 0)}
-			current.children[part] = child
+			child = &Node{make([]fileEntry, 0), part, current}
+			current.children = append(current.children,
+				fileEntry{part, child})
 			current = child
 		}
 	}
+
+	return current
 }
 
 // DeleteAt deletes a directory and its subdirectories/files from the tree
@@ -63,24 +93,40 @@ func (t *Node) DeleteAt(path string) error {
 	parts := pathToParts(path)
 	current := *t
 	for _, part := range parts[:len(parts)-1] {
-		if child, ok := current.children[part]; ok {
-			current = child
+		if child, ok := current.findFile(part); ok {
+			current = *child
 		} else {
 			return ErrInvalidPath{path}
 		}
 	}
 
-	if _, ok := current.children[parts[len(parts)-1]]; !ok {
+	ok := current.deleteFile(parts[len(parts)-1])
+	if !ok {
 		return ErrInvalidPath{path}
 	}
-
-	delete(current.children, parts[len(parts)-1])
 	return nil
+}
+
+func (t Node) GetPath() string {
+	parts := make([]string, 0, 10) // faster?
+	current := &t
+
+	for ; current.parent != nil; current = current.parent {
+		parts = append(parts, current.part)
+	}
+
+	var builder strings.Builder
+	for i := len(parts) - 1; i >= 0; i-- {
+		builder.WriteString("/")
+		builder.WriteString(parts[i])
+	}
+
+	return builder.String()
 }
 
 // New returns a new Node
 func New() *Node {
-	return &Node{make(map[string]Node, 0)}
+	return &Node{make([]fileEntry, 0), "", nil}
 }
 
 func pathToParts(path string) []string {
